cf1de5d2dbc6ad1ce9ba3e1d9f5101a4369920d1
enigma0Z <enigma.0ZA@gmail.com>
2021-06-29 13:42:15 -0400

    Update LibraryClass.groovy

--- src/com/enigma0z/demo/LibraryClass.groovy
+++ src/com/enigma0z/demo/LibraryClass.groovy
@@ -5,6 +5,9 @@ class LibraryClass extends AbstractBaseClass {
 
     LibraryClass(Object pipeline, String value) {
         this.pipeline = pipeline
+        
+        // Stacktrace on the next line:
+        // org.codehaus.groovy.runtime.typehandling.GroovyCastException: Cannot cast object 'com.enigma0z.demo.OtherLibraryClass@34841a9f' with class 'com.enigma0z.demo.OtherLibraryClass' to class 'com.enigma0z.demo.OtherLibraryClass'
         this.otherLibraryClass = new OtherLibraryClass(pipeline, value)
     }
 
